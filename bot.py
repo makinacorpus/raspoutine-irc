49a49cec38fdd1d5074afeaf8fbb72cc5bfa6e2f
Sebastien Corbin <seb.corbin@gmail.com>
2014-10-24 18:23:32 +0200

    Allow reloading bot

--- bot.py
+++ bot.py
@@ -3,11 +3,12 @@
 
 # Import some necessary libraries.
 import socket
+import subprocess
 
 # Some basic variables used to configure the bot
 server = "chat.freenode.net"  # Server
 channel = "#makinacorpus"  # Channel
-botnick = "raspoutine"  # nickname
+botnick = "raspoutine-test"  # nickname
 
 
 def ping():  # This is our first function! It will respond to server Pings.
@@ -33,13 +34,32 @@ ircsock.send("NICK " + botnick + "\n")  # here we actually assign the nick to th
 
 joinchan(channel)  # Join the channel using the functions we previously defined
 
+listen = False
+
 while 1:  # Be careful with these! it might send you to an infinite loop
     ircmsg = ircsock.recv(2048)  # receive data from the server
     ircmsg = ircmsg.strip('\n\r')  # removing any unnecessary linebreaks.
     print(ircmsg)  # Here we print what's coming from the server
+
+    # Begin to listen only when MOTD is done
+    if ircmsg.find(":End of /MOTD command") != -1:
+        listen = True
+        continue
 
     if ircmsg.find(":Hello " + botnick) != -1:  # If we can find "Hello Mybot" it will call the function hello()
         hello()
 
     if ircmsg.find("PING :") != -1:  # if the server pings us then we've got to respond!
         ping()
+
+    if ircmsg.find("PRIVMSG raspoutine-test :reload") != -1:
+        # Fetch latest commits
+        subprocess.call(["git", "fetch"])
+
+        # See if we have changes
+        commits = subprocess.check_output(["git", "log", "HEAD...master", "--pretty-format:%b"])
+
+        # Pull latest content
+        subprocess.call(["git", "pull"])
+        # Reload script
+        execfile(__file__)
